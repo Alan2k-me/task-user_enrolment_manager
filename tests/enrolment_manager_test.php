873bb14ad3b9886dd7ac0d47972aa50dffa99005
Alan Thomas <alan2k.work@gmail.com>
2025-06-09 13:25:24 +1000

    test: add unit tests to get courses a user is enrolled in

--- tests/enrolment_manager_test.php
+++ tests/enrolment_manager_test.php
@@ -6,7 +6,7 @@ use App\user;
 use App\course;
 use App\enrolment_manager;
 
-// Test the enrolment_manager class.
+//Test the enrolment_manager class.
 class enrolment_manager_test extends TestCase {
 
     // @var enrolment_manager Instance of enrolment_manager.
@@ -15,7 +15,7 @@ class enrolment_manager_test extends TestCase {
     /*
     Set up the test data and instantiate the manager.
     @return void
-     */
+    */
     protected function setUp(): void {
         $users = [
             new user(1, "Alice"),
@@ -28,13 +28,13 @@ class enrolment_manager_test extends TestCase {
         $this->manager = new enrolment_manager($users, $courses);
     }
 
-    // Test a user can be enrolled.
+    //Test a user can be enrolled.
     public function test_enrol_user_successfully(): void {
         $this->manager->enrol_user(1, 100);
         $courses = $this->manager->get_user_courses(1);
         $this->assertCount(1, $courses);
     }
-
+    
     //Test duplicate enrolment throws error.
     public function test_duplicate_enrolment_throws_exception(): void {
         $this->manager->enrol_user(1, 100);
@@ -53,7 +53,7 @@ class enrolment_manager_test extends TestCase {
         $this->expectException(\InvalidArgumentException::class);
         $this->manager->enrol_user(1, 999);
     }
-
+    
     //Test a user can be unenrolled.
     public function test_unenrol_user_successfully(): void {
         $this->manager->enrol_user(2, 200);
@@ -79,7 +79,12 @@ class enrolment_manager_test extends TestCase {
         $this->expectException(\InvalidArgumentException::class);
         $this->manager->unenrol_user(1, 999);
     }
-
-    // Add more tests: duplicate enrol, invalid ID, unenrol, etc.
+    
+    //Test getting courses for a user not enrolled returns empty array.
+    public function test_get_courses_for_user_with_no_enrolments(): void {
+        $courses = $this->manager->get_user_courses(2);
+        $this->assertIsArray($courses);
+        $this->assertEmpty($courses);
+    }
 }
 ?>
